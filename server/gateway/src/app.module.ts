88abdd7423cd6766dc9233a0576f3da281c746ad
Gilbert <gilwong3105@gmail.com>
2022-04-18 00:35:07 -0700

    Re add gateway

---
+++ server/gateway/src/app.module.ts
@@ -0,0 +1,25 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        playground: true,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:5000/graphql' },
+            { name: 'matches', url: 'http://localhost:5001/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
+})
+export class AppModule {}
